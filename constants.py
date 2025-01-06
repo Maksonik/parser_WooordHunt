a2d204edd5d5b8b409948d161502c87d52d56e46
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2025-01-06 16:21:58 +0500

    add ruff in poetry

---
+++ constants.py
@@ -0,0 +1,14 @@
+WOOORDHUNT_URL = 'https://wooordhunt.ru/'
+WOOORDHUNT_WORD_URL = 'https://wooordhunt.ru/word/{word}'
+
+PART_OF_SPEECH = {
+    "прилагательное": 'adjectival',
+    'существительное': 'noun',
+    'глагол': 'verb',
+    'союз': 'alliance',
+    'наречие': 'adverb',
+    'местоимение': 'pronoun',
+    'прочее': 'other',
+    'предлог': 'pretext',
+    'междометие': 'interjection',
+}
