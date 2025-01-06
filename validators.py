f576306884fcf046656ae26100b2d95a7f456ed2
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2025-01-06 16:55:07 +0500

    fix validators.py

--- validators.py
+++ validators.py
@@ -1,8 +1,12 @@
 from constants import WOOORDHUNT_WORD_URL
 
 
-def _remake_url_word_in_validated(url_word):
-    """Видоизменить ссылку на правильную"""
-    if url_word.startswith(("https://", "wooordhunt.ru", "www.wooordhunt.ru")):
-        return url_word
-    return WOOORDHUNT_WORD_URL.format(word=url_word)
+def _get_validated_url(word: str) -> str:
+    """
+    Get the correct url to a word in wooordhunt.ru
+    :param word: Url of word or word. Example (www.wooordhunt.ru/word/get or get)
+    :return: url of word
+    """
+    if word.startswith(("https://", "wooordhunt.ru", "www.wooordhunt.ru")):
+        return word
+    return WOOORDHUNT_WORD_URL.format(word=word)
